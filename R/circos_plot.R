952ec800783185eb7c0245aecafb0b40bca4af8b
Anthony Cillo <arc85@pitt.edu>
2019-09-23 15:58:33 -0400

    Add two sample circos plotting

--- R/circos_plot.R
+++ R/circos_plot.R
@@ -1,10 +1,8 @@
 #' Creates a circos plot from the list of ligands and receptors
 #'
-#' @param interactions.list The output from the interactions_list_wrapper. Consists of a list of ligands and receptors in each cell type from each sample group
+#' @param interactions1 List of ligands and receptors and their associated cell types (i.e. the output from one sample group from the putative_interactions function). If interactions2 is NULL, then this will only plot interactions found in the list of ligands and receptors from the sample group specified.
 #'
-#' @param sample.name Name of the sample group to plot from the interactions.list
-#'
-#' @param control.group Optional name of the control group to compare interactions in the sample group to. Default is false
+#' @param interactions2 List of ligands and receptors and their associated cell types (i.e. the output from a second sample group from the putative_interactions function). Default is NULL, but if this value is non-null, will plot both sets of interactions present in "interactions1" and "interactions2". This group is considered the "control" group if present, and unique ligand/receptor interactions from "interactions1" will be plotted.
 #'
 #' @param ligand.col Color of the bars defining the ligands
 #'
@@ -14,17 +12,17 @@
 #'
 #' @param unique.vs.control.interactions.col Optional color of the links connecting the ligand and receptor interactions that are unique between the sample group and the control
 #'
-#' @param metadata.matrix Dataframe containing the metadata from the expression dataset
-#'
 #' @return Generates a circos plot connecting ligands and receptors across cell types for a given sample group
 #'
 #' @export
 
 circos_plot <- function(interactions1,interactions2=NULL,clusters,ligand.col="#C8A757",receptor.col="#70BC71",interactions.col="lightgray",unique.vs.control.interactions.col="darkorange") {
 
+  clusters <- levels(clusters)
+
   if (is.null(interactions2)) {
 
-  interactions.list <- interactions1
+  interactions.list <- list(interactions1)
 
   } else {
 
@@ -34,12 +32,44 @@ circos_plot <- function(interactions1,interactions2=NULL,clusters,ligand.col="#C
 
 	#Setup overall circos plot
 
-  non.neg.int <- sapply(interactions.list,function(x) !is.null(x[[1]]))
-	all.lig.rec.interactions <- interactions.list[non.neg.int]
+  if (length(interactions.list)==1) {
 
-	ligands <- unique(sapply(strsplit(names(all.lig.rec.interactions),split="_"),`[[`,1))
+  non.neg.int <- sapply(sapply(interactions.list,function(y) y),function(x) !is.null(x[[1]]))
+	shared.int <- interactions.list[[1]][non.neg.int]
 
-	receptors <- unique(sapply(strsplit(names(all.lig.rec.interactions),split="_"),`[[`,2))
+  ligands <- unique(sapply(strsplit(names(shared.int),split="_"),`[[`,1))
+
+  receptors <- unique(sapply(strsplit(names(shared.int),split="_"),`[[`,2))
+
+  } else {
+
+    all.int <- c(names(interactions.list[[1]]),names(interactions.list[[2]]))
+    non.neg.int <- sapply(sapply(interactions.list,function(y) y),function(x) !is.null(x[[1]]))
+    all.int <- unique(all.int[non.neg.int])
+
+    group1.pos <- sapply(interactions.list[[1]],function(x) !is.null(x[[1]]))
+    group2.pos <- sapply(interactions.list[[2]],function(x) !is.null(x[[1]]))
+
+    shared.names <- intersect(names(interactions.list[[1]])[group1.pos],names(interactions.list[[2]])[group2.pos])
+
+    group1.shared <- interactions.list[[1]][shared.names]
+    group2.shared <- interactions.list[[2]][shared.names]
+    shared.int1 <- mapply(union,sapply(group1.shared,function(x) x[[1]]),sapply(group2.shared,function(x) x[[1]]),SIMPLIFY=FALSE)
+    shared.int2 <- mapply(union,sapply(group1.shared,function(x) x[[2]]),sapply(group2.shared,function(x) x[[2]]),SIMPLIFY=FALSE)
+
+    shared.int <- vector("list",length=length(shared.names))
+    for (i in 1:length(shared.int)) {
+      shared.int[[i]] <- list("ligand.cells"=shared.int1[[i]],"receptor.cells"=shared.int2[[i]])
+    }
+    names(shared.int) <- shared.names
+
+    group1.uni <- setdiff(names(interactions.list[[1]])[group1.pos],names(interactions.list[[2]])[group2.pos])
+
+    ligands <- unique(sapply(strsplit(all.int,split="_"),`[[`,1))
+
+    receptors <- unique(sapply(strsplit(all.int,split="_"),`[[`,2))
+
+  }
 
 	circos.data <- data.frame(matrix(data=0,nrow=length(clusters)*length(c(ligands,receptors)),ncol=3))
 	colnames(circos.data) <- c("cell.type","genes","ligand.rec")
@@ -71,16 +101,17 @@ circos_plot <- function(interactions1,interactions2=NULL,clusters,ligand.col="#C
 		}
 	)
 
-	#Draw links
 
-	for (i in 1:length(names(all.lig.rec.interactions))) {
-		link <- names(all.lig.rec.interactions)[i]
+	#Draw links - shared
+
+	for (i in 1:length(names(shared.int))) {
+		link <- names(shared.int)[i]
 
 		lig.link <- sapply(strsplit(link,split="_"),`[[`,1)
 		rec.link <- sapply(strsplit(link,split="_"),`[[`,2)
 
-		link.1 <- all.lig.rec.interactions[[i]][["ligand.cells"]]
-		link.2 <- all.lig.rec.interactions[[i]][["receptor.cells"]]
+		link.1 <- shared.int[[i]][["ligand.cells"]]
+		link.2 <- shared.int[[i]][["receptor.cells"]]
 
 		for (a in 1:length(link.1)) {
 
@@ -99,6 +130,10 @@ circos_plot <- function(interactions1,interactions2=NULL,clusters,ligand.col="#C
 	}
 }
 
-print("Plotted interactions")
+if (length(interactions.list)==1) {
+  print("Plotted interactions")
+} else {
+  print("Plotted shared interactions")
+}
 
 }
