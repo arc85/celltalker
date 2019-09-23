b968dc549c3cdca0e993bb769f57d9c93890c7b7
Anthony Cillo <arc85@pitt.edu>
2019-09-23 12:51:25 -0400

    Add simple circos plotting function

--- R/circos_plot.R
+++ R/circos_plot.R
@@ -20,74 +20,50 @@
 #'
 #' @export
 
-circos_plot <- function(interactions.list,sample.name,control.group=FALSE,ligand.col="#C8A757",receptor.col="#70BC71",interactions.col="lightgray",unique.vs.control.interactions.col="darkorange",metadata.matrix) {
+circos_plot <- function(interactions1,interactions2=NULL,clusters,ligand.col="#C8A757",receptor.col="#70BC71",interactions.col="lightgray",unique.vs.control.interactions.col="darkorange") {
 
-	interactions.list <- interactions.list[[2]]
+  if (is.null(interactions2)) {
 
-	cell.types <- levels(as.factor(metadata.matrix$cell.types))
+  interactions.list <- interactions1
+
+  } else {
+
+    interactions.list <- list(interactions1,interactions2)
+
+  }
 
 	#Setup overall circos plot
 
-	all.lig.rec.interactions <- unlist(unname(sapply(interactions.list,function(x) names(x))))
-	unique.lig.rec.interactions <- unique(all.lig.rec.interactions)
+  non.neg.int <- sapply(interactions.list,function(x) !is.null(x[[1]]))
+	all.lig.rec.interactions <- interactions.list[non.neg.int]
 
-	ligands <- unique(sapply(strsplit(unique.lig.rec.interactions,split="_"),`[[`,1))
+	ligands <- unique(sapply(strsplit(names(all.lig.rec.interactions),split="_"),`[[`,1))
 
-	receptors <- unique(sapply(strsplit(unique.lig.rec.interactions,split="_"),`[[`,2))
+	receptors <- unique(sapply(strsplit(names(all.lig.rec.interactions),split="_"),`[[`,2))
 
-	circos.data <- data.frame(matrix(data=0,nrow=length(cell.types)*length(c(ligands,receptors)),ncol=3))
+	circos.data <- data.frame(matrix(data=0,nrow=length(clusters)*length(c(ligands,receptors)),ncol=3))
 	colnames(circos.data) <- c("cell.type","genes","ligand.rec")
 
 	num.lig.rec <- sum(length(ligands),length(receptors))
 
-	seq1 <- seq(1,num.lig.rec*length(cell.types),num.lig.rec)
-	seq2 <- seq(num.lig.rec,num.lig.rec*length(cell.types)+1,num.lig.rec)
+	seq1 <- seq(1,num.lig.rec*length(clusters),num.lig.rec)
+	seq2 <- seq(num.lig.rec,num.lig.rec*length(clusters)+1,num.lig.rec)
 
 	for (i in 1:length(seq2)) {
-		circos.data[seq1[i]:seq2[i],1] <- cell.types[i]
+		circos.data[seq1[i]:seq2[i],1] <- clusters[i]
 		circos.data[seq1[i]:seq2[i],2] <- c(ligands,receptors)
 		circos.data[seq1[i]:seq2[i],3] <- c(rep("lig",length(ligands)),rep("rec",length(receptors)))
 	}
 
-	circos.data$ref.num <- seq(1,num.lig.rec*length(cell.types),1)
+	circos.data$ref.num <- seq(1,num.lig.rec*length(clusters),1)
 
-		plot.interaction.circos <- function(interaction.list,circos.data,sample.to.plot,control.group,ligands,receptors,ligand.col,receptor.col,nonunique.interactions) {
-
-	#Select sample group
-
-	interactions.to.plot <- interactions.list[[sample.to.plot]]
-
-	if (!control.group==FALSE) {
-
-	#Select control group
-
-	control.group.for.comp <- interactions.list[[control.group]]
-
-	#Identify unique interactions vs control interactions
-
-	control.group.names <- names(control.group.for.comp)
-	unique.v.control.group.names <- setdiff(names(interactions.to.plot),control.group.names)
-	unique.v.control.interactions <- interactions.to.plot[unique.v.control.group.names]
-
-	#Identify shared interactions
-
-	nonunique.interactions.names <- setdiff(names(interactions.to.plot),unique.v.control.interactions)
-	nonunique.interactions <- interactions.to.plot[nonunique.interactions.names]
-
-	} else {
-
-	#Identify interactions
-
-	nonunique.interactions <- interactions.to.plot
-
-	}
 
 	#Plot circos
 
 	circos.par("cell.padding"=c(0.02,0,0.02,0),"track.height"=0.1)
 	circos.initialize(factors=circos.data$cell.type,x=seq(1,nrow(circos.data),1))
 
-	circos.track(factors=circos.data$cell.types,ylim=c(0,1),
+	circos.track(factors=circos.data$clusters,ylim=c(0,1),
 		panel.fun=function(x,y) {
 			circos.text(CELL_META$xcenter,CELL_META$cell.ylim[2] + uy(5,"mm"),CELL_META$sector.index)
 			circos.rect(xleft=CELL_META$xlim[1],ybottom=CELL_META$cell.ylim[1],xright=CELL_META$xlim[1]+length(ligands),ytop=CELL_META$cell.ylim[2],col=ligand.col)
@@ -97,18 +73,14 @@ circos_plot <- function(interactions.list,sample.name,control.group=FALSE,ligand
 
 	#Draw links
 
-	if (length(nonunique.interactions)==0) {
-		print("No interactions present")
-	} else {
-
-	for (i in 1:length(names(nonunique.interactions))) {
-		link <- names(nonunique.interactions)[i]
+	for (i in 1:length(names(all.lig.rec.interactions))) {
+		link <- names(all.lig.rec.interactions)[i]
 
 		lig.link <- sapply(strsplit(link,split="_"),`[[`,1)
 		rec.link <- sapply(strsplit(link,split="_"),`[[`,2)
 
-		link.1 <- nonunique.interactions[[i]][["ligand.cells"]]
-		link.2 <- nonunique.interactions[[i]][["receptor.cells"]]
+		link.1 <- all.lig.rec.interactions[[i]][["ligand.cells"]]
+		link.2 <- all.lig.rec.interactions[[i]][["receptor.cells"]]
 
 		for (a in 1:length(link.1)) {
 
@@ -128,52 +100,5 @@ circos_plot <- function(interactions.list,sample.name,control.group=FALSE,ligand
 }
 
 print("Plotted interactions")
-
-}
-
-	if (!control.group==F) {
-
-	#Draw links unique vs control
-
-	if (length(unique.v.control.interactions)==0) {
-		print("No unique vs HD interactions present")
-	} else {
-
-	for (i in 1:length(names(unique.v.control.interactions))) {
-		link <- names(unique.v.control.interactions)[i]
-
-		lig.link <- sapply(strsplit(link,split="_"),`[[`,1)
-		rec.link <- sapply(strsplit(link,split="_"),`[[`,2)
-
-		link.1 <- unique.v.control.interactions[[i]][["ligand.cells"]]
-		link.2 <- unique.v.control.interactions[[i]][["receptor.cells"]]
-
-		for (a in 1:length(link.1)) {
-
-		circos.sub1 <- subset(circos.data,cell.type==link.1[a])
-
-			for (z in 1:length(link.2)) {
-
-				circos.sub2 <- subset(circos.data,cell.type==link.2[z])
-
-				ref.num1 <- circos.sub1$ref.num[which(circos.sub1$gene==lig.link)]
-				ref.num2 <- circos.sub2$ref.num[which(circos.sub2$gene==rec.link)]
-
-				circos.link(link.1[a],ref.num1,link.2[z],ref.num2,col=unique.vs.control.interactions.col,h.ratio=0.2)
-		}
-
-	}
-
-}
-
-print("Plotted unique vs control interactions")
-
-}
-
-}
-
-}
-
-plot.interaction.circos(interactions.list,circos.data,sample.name,control.group,ligands,receptors,ligand.col,receptor.col)
 
 }
