65ca8414fc5249fab57ce1bf0551b525f3208827
Anthony Cillo <arc85@pitt.edu>
2022-03-14 15:27:18 -0400

    Create ligand_receptor_scramble documentation and test

--- tests/testthat/test-ligand_receptor_scramble.R
+++ tests/testthat/test-ligand_receptor_scramble.R
@@ -1,5 +1,6 @@
-## Test formatting of ligand and receptor means
+## Test formatting of ligand and receptor scramble
 # Create dataset
+
 filtered_lig_rec_t <- Matrix::t(filtered_lig_rec) %>% as.matrix()
 lig_rec_meta <- cbind(filtered_lig_rec_t,overall_metadata)
 
@@ -20,20 +21,21 @@ pbmc_rec_means <- pbmc_lig_rec_means[rownames(pbmc_lig_rec_means) %in% unique(ra
 
 pbmc_joint_means <- ligand_receptor_means(ligand_mean_dataframe=pbmc_lig_means,receptor_mean_dataframe=pbmc_rec_means,ligand_receptor_pairs=ramilowski_pairs,number_cell_types=10)
 
+pbmc_joint_scramble <- ligand_receptor_scramble(pbmc_joint_means,pbmc_lig_means,pbmc_rec_means,ramilowski_pairs,number_cell_types=10,scramble_times=10)
 
 # Tests
 test_that("structure of result", {
-  expect_type(pbmc_joint_means,"list")
+  expect_type(pbmc_joint_scramble,"list")
 })
 
 test_that("dimensions", {
-  expect_equal(dim(pbmc_joint_means),c(100,62))
+  expect_equal(dim(pbmc_joint_scramble),c(6100,4))
 })
 
 test_that("expected value in row 1 column 1", {
-  expect_match(pbmc_joint_means[1,1] %>% pull(),"CD8 T cells_CD8 T cells")
+  expect_match(pbmc_joint_scramble[1,1] %>% pull(),"CD8 T cells_CD8 T cells")
 })
 
-test_that("value of row 100 column 1", {
-  expect_match(pbmc_joint_means[100,1] %>% pull(),"RBCs_RBCs")
+test_that("value of row 6100 column 1", {
+  expect_match(pbmc_joint_scramble[6100,1] %>% pull(),"RBCs_RBCs")
 })
